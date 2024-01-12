c5451cd8b63bd2ea9d8e285262540794073e615d
Robert Dorna <rdorna8@gmail.com>
2024-01-12 22:39:46 +0000

    feat: add swagger (closes #7)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { writeFile } from 'node:fs/promises';
+import * as yaml from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Bodypace personal data server API')
+    .setDescription(
+      "All endpoints are here, it's not a lot because the server is indeed simple",
+    )
+    .setVersion('0.1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    customfavIcon: 'https://bodypace.org/favicon.ico',
+    customSiteTitle: 'Bodypace API documentation - personal data server',
+  });
+  await writeFile(
+    'docs/openapi.yaml',
+    yaml.stringify(document, { aliasDuplicateObjects: false }),
+    'utf-8',
+  );
+  if (process.argv.includes('--only-generate-openapi-spec')) {
+    console.log('OpenAPI docs generated, exiting because of CLI arg');
+    await app.close();
+    return;
+  }
   await app.listen(8080);
 }
 void bootstrap();
--- src/modules/accounts/dto/account-credentials.dto.ts
+++ src/modules/accounts/dto/account-credentials.dto.ts
@@ -1,10 +1,21 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AccountCredentials {
+  @ApiProperty({
+    description: 'Unique username',
+    example: 'john_doe',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty({
+    description: 'User password',
+    example: 'some_strong_password',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
